00e2b84150eb7e79d641f3a4000eefb69149d1a1
mark.weigelt <mark.weigelt@codehaus.de>
2016-11-23 21:01:13 +0100

    Bewegungsbegrenzung links/rechts für Ship

--- SpaceInvaders/src/org/blueberry/spaceinvaders/Ship.java
+++ SpaceInvaders/src/org/blueberry/spaceinvaders/Ship.java
@@ -1,17 +1,24 @@
 package org.blueberry.spaceinvaders;
 
+import static org.blueberry.spaceinvaders.SpaceInvaders.HEIGHT;
+import static org.blueberry.spaceinvaders.SpaceInvaders.WIDTH;
 import javafx.scene.image.Image;
 
-
 public class Ship extends Sprite {
+    protected static final double LEFT_BOUNDARY = 0;
+    protected static double rightBoundary;
     SpaceInvaders ship;
-    public Ship (SpaceInvaders iShip, Image img, double xLocation, double yLocation){
+    
+    public Ship (SpaceInvaders iShip, Image img, double xLocation, double yLocation) {
         super(img, xLocation, yLocation);
         ship = iShip;
+        rightBoundary = WIDTH - img.getWidth();
     }
+    
     @Override
     public void update() {
         setXPosition();
+        setBoundaries();
         moveShip(positionX, positionY);
     }
     
@@ -24,9 +31,20 @@ public class Ship extends Sprite {
         }
     }
     
+    private void setBoundaries() {
+        if(positionX >= rightBoundary) {
+            positionX = rightBoundary;
+        }
+        if(positionX <= LEFT_BOUNDARY) {
+            positionX = LEFT_BOUNDARY;
+        }
+    }
+    
     private void moveShip(double x, double y) {
         imageView.setTranslateX(x);
         imageView.setTranslateY(y);
     }
+
+    
    
 }
